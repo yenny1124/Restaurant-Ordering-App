aa44ac19ea1fbb6044224123cc35ff13717ab877
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-02-17 01:07:41 -0800

    footer outline added

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -1,3 +1,21 @@
+import "./footer.css";
+import { Architects_Daughter } from "next/font/google";
+const architectsDaughter = Architects_Daughter({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 export default function Footer() {
-  return <footer>The footer goes here</footer>;
+  return (
+    <footer>
+      <div className={`footer-content ${architectsDaughter.className}`}>
+        <div>Address</div>
+        <div>Follow Us!</div>
+        <div>Hours</div>
+      </div>
+
+      <div className="copyright">
+        <p>&copy;All Rights Reserved By Rolls and Rolls </p>
+      </div>
+    </footer>
+  );
 }
