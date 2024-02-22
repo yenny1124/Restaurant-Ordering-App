fc93949962c7e2a5193dfc07bb643ab08d93c799
anthonyhu64 <anthonyhu64@gmail.com>
2024-02-21 21:53:43 -0800

    some footer work

--- src/app/components/footer/Footer.tsx
+++ src/app/components/footer/Footer.tsx
@@ -8,9 +8,16 @@ export default function Footer() {
   return (
     <footer>
       <div className={`footer-content ${architectsDaughter.className}`}>
-        <div>Address</div>
-        <div>Opening Hours</div>
-        <div>Contacts</div>
+        <div>
+          <h1>Address</h1>
+          <address>11011 NE 12th St c1, Bellevue, WA 98004</address>
+        </div>
+        <div>
+          <h1>Opening Hours</h1>
+        </div>
+        <div>
+          <h1>Contacts</h1>
+        </div>
       </div>
 
       <div className="copyright">
