83a7b022a003fb0d7df1c00882e7dff666389a45
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-02-23 23:52:57 -0800

    style: added sizing

--- src/app/menu/categories/components/itemcard/ItemCard.tsx
+++ src/app/menu/categories/components/itemcard/ItemCard.tsx
@@ -2,7 +2,7 @@ import "./itemcard.css";
 export function ItemCard(props: { itemName: string; itemDescription: string }) {
   return (
     <div className="item-card">
-      <p>{props.itemName}</p>
+      <h2>{props.itemName}</h2>
       <p>{props.itemDescription}</p>
     </div>
   );
