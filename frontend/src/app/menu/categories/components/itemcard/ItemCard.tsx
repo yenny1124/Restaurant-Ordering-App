9ae10562e632d24184f285a33d40e5f01030620d
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-02-26 16:55:29 -0800

    refactor: moved to correct folder location

--- frontend/src/app/menu/categories/components/itemcard/ItemCard.tsx
+++
@@ -1,9 +0,0 @@
-import "./itemcard.css";
-export function ItemCard(props: { name: string; desc: string }) {
-  return (
-    <div className="item-card">
-      <h2>{props.name}</h2>
-      <p>{props.desc}</p>
-    </div>
-  );
-}
