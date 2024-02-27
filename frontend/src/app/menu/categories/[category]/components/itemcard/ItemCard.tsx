dcb378461efa8c245dfccbd2ee5a033c65547091
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-02-26 19:23:03 -0800

    feat: added modals for each item

--- frontend/src/app/menu/categories/[category]/components/itemcard/ItemCard.tsx
+++ frontend/src/app/menu/categories/[category]/components/itemcard/ItemCard.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useState } from "react";
 import "./itemcard.css";
 import Image from "next/image";
 
@@ -9,12 +11,32 @@ type ItemTable = {
   prices: Array<number>;
 };
 export function ItemCard(props: ItemTable) {
+  const [modalState, setModalState] = useState(false);
+  function modalOn() {
+    setModalState(true);
+  }
+  function modalOff() {
+    setModalState(false);
+  }
   return (
     <div className="item-card">
+      <button className="button open-modal" onClick={modalOn}>
+        open modal
+      </button>
       <h2>{props.name}</h2>
-      <p>{props.desc}</p>
+      <div className="item-card-image">
+        <img src={props.img} alt={props.name} width="100%" />
+      </div>
       <p>{props.prices}</p>
-      <img src={props.img} alt={props.name} width="100%" />
+      <dialog className="modal" open={modalState}>
+        <h2>{props.name}</h2>
+        <div className="item-card-image">
+          <img src={props.img} alt={props.name} width="200px" height="200px" />
+        </div>
+        <p>{props.prices}</p>
+        <p>{props.desc}</p>
+        <button className=" button close-button" onClick={modalOff}></button>
+      </dialog>
     </div>
   );
 }
