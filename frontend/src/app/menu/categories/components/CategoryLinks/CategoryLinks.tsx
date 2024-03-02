adef140eb5f2275a85929c8613b847d934267d02
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-03-02 00:05:34 -0800

    refactor: removed unnecessary forwardRef

--- frontend/src/app/menu/categories/components/CategoryLinks/CategoryLinks.tsx
+++ frontend/src/app/menu/categories/components/CategoryLinks/CategoryLinks.tsx
@@ -1,30 +1,13 @@
 "use client";
 
-import React, { ReactComponentElement, ReactHTML, useRef } from "react";
+import React from "react";
 import "./categorylinks.css";
 import MenuNavbarLink from "../menunavbar/MenuNavLink";
-import {
-  faCaretRight,
-  faCaretLeft,
-  faFont,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-import {} from "@fortawesome/free-solid-svg-icons";
 
 export default function CategoryLinks(props: {
   categories: [];
   navType: string;
 }) {
-  let ref = useRef<Array<HTMLDivElement | null>>([]);
-  function scrollToLink(i: number) {
-    ref.current[i]?.scrollIntoView({
-      behavior: "smooth",
-      block: "center",
-      inline: "nearest",
-    });
-  }
-
   function createCategoryLinks(categories: []) {
     let categoryElements: any = [];
     categoryElements = categories.map(
@@ -34,9 +17,6 @@ export default function CategoryLinks(props: {
           {
             category: `${category.name}`,
             key: `menu-navbar-link-${category._id}`,
-            ref: (element) => (ref.current[i] = element),
-            scrollingFunction: scrollToLink,
-            refIndex: i,
           },
           `${category.name}`
         );
--- frontend/src/app/menu/categories/components/menunavbar/MenuNavLink.tsx
+++ frontend/src/app/menu/categories/components/menunavbar/MenuNavLink.tsx
@@ -1,50 +1,45 @@
 "use client";
 
 import "./menunavbar.css";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { forwardRef } from "react";
 
-type LinkReference = {
-  category: string;
-  scrollingFunction: any;
-  refIndex: number;
+const MenuNavbarLink = (props: { category: string }) => {
+  const ref = useRef<HTMLAnchorElement | null>(null);
+  const [selectedClass, setClass] = useState("");
+  const pathname = usePathname();
+  const lastPath = pathname.substring(
+    pathname.lastIndexOf("/") + 1,
+    pathname.length
+  );
+
+  const categoryPathName = props.category
+    .replaceAll(" ", "-")
+    .replaceAll("/", "&");
+
+  useEffect(() => {
+    if (lastPath == categoryPathName) {
+      setClass("current-category");
+      ref.current?.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+        inline: "nearest",
+      });
+    } else {
+      setClass("");
+    }
+  }, [lastPath, props, categoryPathName]);
+
+  return (
+    <Link
+      href={`/menu/categories/${categoryPathName}`}
+      key={`menu-navbar-link-${categoryPathName}`}
+      className={selectedClass}
+      ref={ref}
+    >
+      {props.category}
+    </Link>
+  );
 };
-
-// managed to get ref off of any, but then there is an issue with passing that to Link's ref. so im leaving it as any
-const MenuNavbarLink = forwardRef<HTMLDivElement, LinkReference>(
-  (props: LinkReference, ref: any) => {
-    const [selectedClass, setClass] = useState("");
-    const pathname = usePathname();
-    const lastPath = pathname.substring(
-      pathname.lastIndexOf("/") + 1,
-      pathname.length
-    );
-
-    const categoryPathName = props.category
-      .replaceAll(" ", "-")
-      .replaceAll("/", "&");
-
-    useEffect(() => {
-      if (lastPath == categoryPathName) {
-        setClass("current-category");
-        props.scrollingFunction(props.refIndex);
-      } else {
-        setClass("");
-      }
-    }, [lastPath, props, categoryPathName]);
-    return (
-      <Link
-        href={`/menu/categories/${categoryPathName}`}
-        key={`menu-navbar-link-${categoryPathName}`}
-        className={selectedClass}
-        ref={ref}
-      >
-        {props.category}
-      </Link>
-    );
-  }
-);
-MenuNavbarLink.displayName = "MenuNavbarLink";
 export default MenuNavbarLink;
