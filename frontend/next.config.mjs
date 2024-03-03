fefa7640ae037b615694ebf0bd6823bf058b984a
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-03-02 20:06:21 -0800

    config: default path is now home

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
