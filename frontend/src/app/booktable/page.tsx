f1f6ba96db1e275e18987a336d64b7d5244352ea
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-03-04 15:52:33 -0800

    feat/style: basic book table page functionality

--- frontend/src/app/booktable/page.tsx
+++ frontend/src/app/booktable/page.tsx
@@ -1,3 +1,55 @@
+import "./booktable.css";
+
 export default function BookTable() {
-  return <div>This is the BookTable page</div>;
+  return (
+    <main className="book-table-page">
+      <div className="book-table-content">
+        <form className="book-table-form">
+          <div>
+            <label className="book-table-input-label" htmlFor="phoneNumber">
+              Phone Number
+              <br />
+            </label>
+            <input
+              type="text"
+              className="book-table-input"
+              id="phoneNumber"
+            ></input>
+          </div>
+          <div>
+            <label className="book-table-input-label">
+              Email
+              <br />
+            </label>
+            <input type="text" className="book-table-input"></input>
+          </div>
+          <div>
+            <label className="book-table-input-label">
+              Party Size
+              <br />
+            </label>
+            <input type="number" className="book-table-input"></input>
+          </div>
+          <div>
+            <label className="book-table-input-label">
+              Date
+              <br />
+            </label>
+            <input type="date" className="book-table-input"></input>
+          </div>
+          <div>
+            <label className="book-table-input-label">
+              Time
+              <br />
+            </label>
+            <input type="time" className="book-table-input"></input>
+          </div>
+          <input type="submit" className="submit-button"></input>
+        </form>
+      </div>
+      <div className="book-table-content">
+        Google Maps embed or some image content should go here
+      </div>
+    </main>
+  );
 }
