5da84f9fa81ca057037b04f19eef4979d1cae656
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-03-06 02:28:03 -0800

    fix: import path was causing syntax error

--- src/app/menu/components/menunavbar/MenuNavbar.tsx
+++ src/app/menu/components/menunavbar/MenuNavbar.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import React from "react";
 import "./menunavbar.css";
-import CategoryLinks from "../categorylinks/CategoryLinks";
+import CategoryLinks from "../categoryLinks/CategoryLinks";
 import {
   faCaretRight,
   faCaretLeft,
