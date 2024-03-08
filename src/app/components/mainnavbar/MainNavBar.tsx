064196576b2ac63a329dcafa33350ee49a5fa156
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-03-07 21:51:48 -0800

    refactor: removed unnecessary layer

--- src/app/components/mainnavbar/MainNavBar.tsx
+++ src/app/components/mainnavbar/MainNavBar.tsx
@@ -1,7 +1,8 @@
 import "./mainnavbar.css";
-import HamburgerMenu from "./HamburgerMenu";
-import MainNavBarLinks from "./MainNavBarLinks";
+import HamburgerMenu from "../hamburgermenu/HamburgerMenu";
 import LogoLink from "../logolink/LogoLink";
+import CartCounter from "../cartcounter/CartCounter";
+import MainNavBarLink from "./MainNavBarLink";
 
 export default function MainNavBar() {
   return (
@@ -14,7 +15,16 @@ export default function MainNavBar() {
           imageUrl="/logo.png"
           altText="Rolls & Rolls Restaurant Logo"
         />
-        <MainNavBarLinks />
+        <nav className={`main-navbar-links`}>
+          <MainNavBarLink path="/home" text="Home" />
+          <MainNavBarLink path="/menu" text="Menu" />
+          <MainNavBarLink path="/about" text="About" />
+          <MainNavBarLink path="/booktable" text="Book Table" />
+          <MainNavBarLink path="/cart" text="Cart">
+            &nbsp;
+            <CartCounter />
+          </MainNavBarLink>
+        </nav>
         <HamburgerMenu />
       </div>
     </header>
