b01fada54a3f104fd4bde0b54d0de8bf828aeab1
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-03-07 22:11:54 -0800

    refactor: delete unused component

--- src/app/components/linkbutton/LinkButton.tsx
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-import "./linkbutton.css";
-export default function LinkButton(props: { link: string; text: string }) {
-  return (
-    <Link className="button-lg" href={props.link}>
-      {props.text}
-    </Link>
-  );
-}
