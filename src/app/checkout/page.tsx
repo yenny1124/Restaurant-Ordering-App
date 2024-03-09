ce0713f2af6226dbc738ac371baf805e0182d3c7
anthonyhucsse <anthonyhuCSSE@gmail.com>
2024-03-08 17:04:34 -0800

    feat: added order complete page

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -5,9 +5,11 @@ import { CartItemType, ItemType } from "../types";
 import ItemizedBill from "../components/cart/ItemizedBill/ItemizedBill";
 import Link from "next/link";
 import "./checkout.css";
-import { clearCart } from "../services/cartservices";
-
+import { clearCart, localCartRefresh } from "../services/cartservices";
+import { useRouter } from "next/navigation";
+import { fetchCartItems } from "../services/fetchservices";
 export default function Checkout() {
+  const router = useRouter();
   const [cartItems, setCartItems] = useState<Array<CartItemType>>([]);
   const [formData, setFormData] = useState({
     name: "",
@@ -70,9 +72,11 @@ export default function Checkout() {
 
       const result = await response.json();
       console.log("Order submitted successfully", result);
+
       setSubmissionStatus(1);
       // Optionally clear the cart after successful order submission
       clearCart(true);
+      router.push("/orderstatus");
       // Navigate to a success page or show a success message
       //navigate(`/order/${result._id}`); // Adjust the route as needed
     } catch (error) {
@@ -84,15 +88,27 @@ export default function Checkout() {
   };
 
   useEffect(() => {
-    getItems();
+    fetchCartItems().then((updatedCartItems) => {
+      setCartItems(updatedCartItems);
+    });
   }, []);
 
   useEffect(() => {
-    window.addEventListener("storage", updateCartItems);
+    window.addEventListener("storage", cartListener);
     return () => {
-      window.removeEventListener("storage", updateCartItems);
+      window.removeEventListener("storage", cartListener);
     };
   }, [cartItems]);
+
+  function cartListener() {
+    const updatedCartItems = localCartRefresh(cartItems);
+    if (!Array.isArray(updatedCartItems) || !updatedCartItems.length) {
+      setCartItems([]);
+
+      return;
+    }
+    setCartItems(updatedCartItems);
+  }
 
   function calculateTotal() {
     let total = 0;
@@ -101,64 +117,6 @@ export default function Checkout() {
     });
     return total;
   }
-  function updateCartItems() {
-    if (typeof localStorage === "undefined") {
-      return;
-    }
-
-    let updatedCartItems: Array<CartItemType> = [];
-    cartItems.forEach((item) => {
-      const key = "cart-item" + item.item._id;
-
-      const value = localStorage.getItem(key);
-
-      if (value === null) {
-        return;
-      }
-      const parsedValue = JSON.parse(value);
-      if (parsedValue === undefined) return;
-      const quantity = parseInt(parsedValue.quantity);
-      updatedCartItems.push({ item: item.item, quantity: quantity });
-      // we are checking each element
-      // we want to see if it exists in localStorage
-      // if it does, we set the quantity to match
-    });
-    setCartItems(updatedCartItems);
-  }
-
-  const getItems = async () => {
-    if (typeof localStorage === "undefined") {
-      return;
-    }
-    const keys = Object.keys(localStorage);
-    let items: Array<CartItemType> = [];
-
-    await Promise.all(
-      keys.map(async (key: string) => {
-        if (!key.includes("cart-item")) return;
-        const stringValue = localStorage.getItem(key);
-        if (stringValue === null) return;
-        const objectValue = JSON.parse(stringValue);
-        let response;
-        if (objectValue === undefined) return;
-
-        response = await fetch(
-          `https://restaurant-ecommerce.onrender.com/api/get/product/${objectValue._id}`
-        );
-
-        if (!response.ok) {
-          console.log(
-            `Could not retrieve ${objectValue._id} item from database`
-          );
-          return;
-        }
-
-        const item = await response.json();
-        items.push({ item, quantity: objectValue.quantity });
-      })
-    );
-    setCartItems(items);
-  };
 
   return (
     <main className="cart-main">
